0e306aceb0b980bbe86e916b1a3e8801b70ac1f9
doxas <eizanco@gmail.com>
2019-05-04 16:40:47 +0900

    add chrome debugging protocol

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 
-const ChromeLauncher = require('chrome-launcher');
+const ChromeLauncher          = require('chrome-launcher');
+const ChromeDebuggingProtocol = require('chrome-remote-interface');
 
 let chrome;
+let protocol;
+let network;
+let page;
+let dom;
+let runtime;
 
 launchChrome()
 .then((ch) => {
     chrome = ch;
-    console.log(`chrome debugging port running on ${chrome.port}`);
-    console.log(chrome);
-    chrome.kill().then(() => {
-        console.error('killed chrome');
+})
+.then(() => {
+    return ChromeDebuggingProtocol({
+        port: chrome.port,
     });
 })
+.then((debuggingProtocol) => {
+    protocol = debuggingProtocol;
+    network  = debuggingProtocol.Network;
+    page     = debuggingProtocol.Page;
+    dom      = debuggingProtocol.DOM;
+    runtime  = debuggingProtocol.Runtime;
+})
 .catch((err) => {
+    throw err;
+})
+.finally(() => {
+    if(protocol != null){
+        console.log('protocol closed');
+        protocol.close();
+    }
     if(chrome != null){
         chrome.kill().then(() => {
-            console.error('killed chrome');
-            throw err;
+            console.error('chrome process killed');
         });
-    }else{
-        throw err;
     }
 });
 
